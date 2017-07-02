04e0099ce4ed8397d00391a7de929d2359ffcbbe
Praveen Kumar <praveen@unbxd.com>
2017-07-02 23:03:46 +0530

    first commmit of the library with basic setup
    Use render highjacking to convert wrapped component into a carousel
    Keep hard coded state of how many slides to show and currently showing slide

---
+++ src/index.js
@@ -0,0 +1,38 @@
+function carouselHOC(WrappedComponent) {
+  return class Enhancer extends WrappedComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
+        slideCount: 2, // # take this from config
+        currentSlide: 0 
+      };
+    }
+
+    onClick(){
+      this.setState(prevState => (
+        { currentSlide: prevState.currentSlide + prevState.slideCount }
+      ))
+    }
+
+    render() {
+      const { currentSlide, slideCount } = this.state;
+      const dom = super.render();
+
+      const slides = dom.props.children.filter((child, counter) => {
+        if(currentSlide <= counter && counter < currentSlide + slideCount ) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+
+      return (
+        <div onClick={_ => this.onClick()} >
+          {slides}
+        </div>
+      )
+    }
+  }
+}
+
+export default carouselHOC;
